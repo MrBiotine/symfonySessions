0e48eb9f3c53171bfbf4d66e80ea1d7f8dbed78a
bruno-PC\bruno <bruno.doue@laposte.net>
2023-10-02 00:52:10 +0200

    fix coueseType

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Course;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
